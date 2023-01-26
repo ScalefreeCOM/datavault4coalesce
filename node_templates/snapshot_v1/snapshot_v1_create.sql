995d79cee1e34eb2735b065a040f7e914067ca77
JPLangner <jlangner@scalefree.com>
2023-01-26 16:06:39 +0100

    Added: snapshot_v1 start_date
    Co-authored-by: Deniz Polat <dpolatscalefree@users.noreply.github.com>

---
+++ node_templates/snapshot_v1/snapshot_v1_create.sql
@@ -0,0 +1,59 @@
+{%- macro snowflake__control_snap_v0(start_date, daily_snapshot_time, sdts_alias) -%}
+
+{%- set start_date = SELECT min("{{ sources[0].columns[0].sourceColumns[0].column.name }}") FROM "{{ storageLocations[0].database }}"."{{ storageLocations[0].schema }}"."{{ sources[0].columns[0].sourceColumns[0].node.name }}" -%}
+{%- set timestamp_format = parameters.datavault4coalesce__timestamp_format() -%}
+{%- set start_date = start_date | replace('00:00:00', daily_snapshot_time) -%}
+
+WITH 
+
+initial_timestamps AS (
+    
+    SELECT
+        DATEADD(DAY, SEQ4(), 
+        TIMESTAMPADD(SECOND, EXTRACT(SECOND FROM TO_TIME('{{ daily_snapshot_time }}')), 
+            TIMESTAMPADD(MINUTE, EXTRACT(MINUTE FROM TO_TIME('{{ daily_snapshot_time }}')), 
+                TIMESTAMPADD(HOUR, EXTRACT(HOUR FROM TO_TIME('{{ daily_snapshot_time }}')), TO_DATE('{{ start_date }}', 'YYYY-MM-DD')))
+                ))::TIMESTAMP AS sdts
+    FROM 
+        TABLE(GENERATOR(ROWCOUNT => 100000))
+    WHERE 
+        sdts <= CURRENT_TIMESTAMP
+    {%- if is_incremental() %}
+    AND sdts > (SELECT MAX({{ sdts_alias }}) FROM {{ this }})
+    {%- endif %}
+
+),
+
+enriched_timestamps AS (
+
+    SELECT
+        sdts as {{ sdts_alias }},
+        TRUE as force_active,
+        sdts AS replacement_sdts,
+        CONCAT('Snapshot ', DATE(sdts)) AS caption,
+        CASE
+            WHEN EXTRACT(MINUTE FROM sdts) = 0 AND EXTRACT(SECOND FROM sdts) = 0 THEN TRUE
+            ELSE FALSE
+        END AS is_hourly,
+        CASE
+            WHEN EXTRACT(MINUTE FROM sdts) = 0 AND EXTRACT(SECOND FROM sdts) = 0 AND EXTRACT(HOUR FROM sdts) = 0 THEN TRUE
+            ELSE FALSE
+        END AS is_daily,
+        CASE
+            WHEN EXTRACT(DAYOFWEEK FROM  sdts) = 2 THEN TRUE
+            ELSE FALSE
+        END AS is_weekly,
+        CASE
+            WHEN EXTRACT(DAY FROM sdts) = 1 THEN TRUE
+            ELSE FALSE
+        END AS is_monthly,
+        CASE
+            WHEN EXTRACT(DAY FROM sdts) = 1 AND EXTRACT(MONTH FROM sdts) = 1 THEN TRUE
+            ELSE FALSE
+        END AS is_yearly,
+        NULL AS comment
+    FROM initial_timestamps
+
+)
+
+SELECT * FROM enriched_timestamps
