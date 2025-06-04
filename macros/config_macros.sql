a8fee187b542949f12537c710bf49ce21c3cb852
bibhushnepal <bnepal@scalefree.com>
2025-06-04 14:00:27 +0200

    fix: use databse_type for ldts instead of parameters

--- macros/config_macros.sql
+++ macros/config_macros.sql
@@ -3,17 +3,17 @@
 {#------------------------------------------------------------------------------------------------------#}
 {%- macro datavault4coalesce__beginning_of_all_times(database_type) -%}
   {%- if database_type | upper == "SNOWFLAKE" -%}
-    0001-01-01T00:00:01
+    "0001-01-01T00:00:01"
   {%- elif database_type | upper == "DATABRICKS" -%}
-    0001-01-01 00:00:01
+    "0001-01-01 00:00:01"
   {%- endif %}
 {%- endmacro -%}
 
 {%- macro datavault4coalesce__end_of_all_times(database_type) -%}
   {%- if database_type | upper == "SNOWFLAKE" -%}
-    8888-12-31T23:59:59
+    "8888-12-31T23:59:59"
   {%- elif database_type | upper == "DATABRICKS" -%}
-    8888-12-31 23:59:59
+    "8888-12-31 23:59:59"
   {%- endif %}
 {%- endmacro -%}
 
@@ -27,8 +27,8 @@
 
 {%- macro datavault4coalesce__timestamp_format(database_type) -%}
   {%- if database_type | upper == "SNOWFLAKE" -%}
-    YYYY-MM-DDTHH24:MI:SS
+    "YYYY-MM-DDTHH24:MI:SS"
   {%- elif database_type | upper == "DATABRICKS" -%}
-    yyyy-MM-dd HH:mm:ss
+    "yyyy-MM-dd HH:mm:ss"
   {%- endif %}
 {%- endmacro -%}
